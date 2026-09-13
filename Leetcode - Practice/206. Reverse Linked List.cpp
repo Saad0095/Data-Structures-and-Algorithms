1e2a20fba3d1d1d956f8607398aed30419eab5bd
Saad Bin Khalid <saadbinkhalid1895@gmail.com>
2026-09-13 13:26:44 +0500

    Solved leetcode problem#206. Reverse Linked List

---
+++ Leetcode - Practice/206. Reverse Linked List.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+//  3 pointer approach ; S.C => O(1) ; T.C => O(n)
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* current = head;
+        ListNode* next = NULL;
+
+        while(current != NULL){
+            next = current -> next;
+            current->next = prev;
+            prev = current;
+            current = next; 
+        }
+        return prev;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
